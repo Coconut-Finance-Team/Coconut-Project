c546c2ab0ada78da7d09929f7afbd75069c3093b
최수연 <tndus5383@gmail.com>
2024-11-06 17:35:51 +0900

    test(#7): Bearer Token을 받아오는 테스트 코드 작성

--- Coconut-Backend/src/test/java/com/coconut/stock_app/service/TokenServiceTest.java
+++ Coconut-Backend/src/test/java/com/coconut/stock_app/service/TokenServiceTest.java
@@ -1,4 +1,21 @@
+package com.coconut.stock_app.service;
+
 import static org.junit.jupiter.api.Assertions.*;
-class TokenServiceTest {
-  
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TokenServiceTest {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Test
+    public void testGetAccessToken() {
+        String token = tokenService.getAccessToken();
+        System.out.println("Access Token: " + token); // 토큰 출력
+        assertNotNull(token, "Access token should not be null");
+    }
 }
