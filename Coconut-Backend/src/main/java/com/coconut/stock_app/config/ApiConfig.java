edf4786993a796d032281efc145bb61ab0274517
최수연 <tndus5383@gmail.com>
2024-11-06 17:33:31 +0900

    feat(#7): 프로퍼티 변수 읽는 Config 파일 작성

--- Coconut-Backend/src/main/java/com/coconut/stock_app/config/ApiConfig.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/config/ApiConfig.java
@@ -1,2 +1,28 @@
-package com.coconut.stock_app.config;public class ApiConfig {
+package com.coconut.stock_app.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiConfig {
+    @Value("${koreainvestment.appKey}")
+    private String appKey;
+
+    @Value("${koreainvestment.appSecret}")
+    private String appSecret;
+
+    @Value("${koreainvestment.apiUrl}")
+    private String apiUrl;
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
 }
