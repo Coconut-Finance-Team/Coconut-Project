aad857c982b704f1e2f0519e893bd245706a3bcd
최수연 <tndus5383@gmail.com>
2024-11-06 17:34:50 +0900

    feat(#7): api test를 위해 시큐리티 없이 모든 엔드포인트 접근 허용

--- Coconut-Backend/src/main/java/com/coconut/stock_app/config/SecurityConfig.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.coconut.stock_app.config;public class SecurityConfig {
+package com.coconut.stock_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("*").permitAll() // 인증 없이 접근 허용
+                                .anyRequest().authenticated()
+                )
+                .csrf().disable(); // CSRF 비활성화 (필요한 경우)
+
+        return http.build();
+    }
 }
