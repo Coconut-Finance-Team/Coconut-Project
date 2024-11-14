75955e47cc96e8f0f981493870c88fbe8767b228
최수연 <tndus5383@gmail.com>
2024-11-14 23:37:34 +0900

    refactor(#46): BaseEntity 코드 변경 및 폴더 수정

--- Coconut-Backend/src/main/java/com/coconut/stock_app/entity/common/BaseEntity.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/entity/common/BaseEntity.java
@@ -1,34 +1,28 @@
 package com.coconut.stock_app.entity.common;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @MappedSuperclass
-public abstract class BaseEntity implements Serializable {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
+    @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
+
