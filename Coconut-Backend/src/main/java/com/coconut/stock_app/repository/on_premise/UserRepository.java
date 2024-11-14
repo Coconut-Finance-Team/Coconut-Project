03045171ead5745b673c2a862fc700da1d754a4e
최수연 <tndus5383@gmail.com>
2024-11-14 23:42:35 +0900

    feat(#46): OAuth2.0 로그인 처리 서비스 코드 작성

--- Coconut-Backend/src/main/java/com/coconut/stock_app/repository/on_premise/UserRepository.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/repository/on_premise/UserRepository.java
@@ -1,9 +1,12 @@
 package com.coconut.stock_app.repository.on_premise;
 
 import com.coconut.stock_app.entity.on_premise.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(String id);
 }
