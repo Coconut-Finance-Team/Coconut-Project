f546bbe28a6e88d475a4c391acb05424654c84ec
최수연 <tndus5383@gmail.com>
2024-11-14 23:43:56 +0900

    feat(#46): OAuth2.0 성공/실패 핸들러 코드 구현

---
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/authentication/oauth/OAuth2LoginFailureHandler.java
@@ -0,0 +1,19 @@
+package com.coconut.stock_app.authentication.oauth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        // 실패 시 React의 로그인 페이지로 리디렉션
+        response.sendRedirect("http://localhost:3000/login?error=true");
+    }
+}
