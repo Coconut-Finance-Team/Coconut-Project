70602a585304b3720b146825633fe823275bca62
jiwon <pg15320@gmail.com>
2024-11-15 10:24:44 +0900

    feat(#19): 배치 저장 기능 구현

--- Coconut-Backend/src/main/java/com/coconut/stock_app/dto/stock/StockChartDTO.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/dto/stock/StockChartDTO.java
@@ -1,5 +1,7 @@
 package com.coconut.stock_app.dto.stock;
 
+import com.coconut.stock_app.entity.cloud.Stock;
+import com.coconut.stock_app.entity.cloud.StockChart;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +10,8 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Data
 @AllArgsConstructor
@@ -15,7 +19,6 @@ import java.time.LocalDateTime;
 @Builder
 public class StockChartDTO {
     private String stockCode;//0
-    private String time;//1
     private BigDecimal currentPrice;//2
     private BigDecimal openPrice;//7
     private BigDecimal highPrice;//8
@@ -24,4 +27,34 @@ public class StockChartDTO {
     private BigDecimal contingentVol;//12
     private BigDecimal accumulatedVol;//13
     private BigDecimal accumulatedAmount; //14
+
+
+    /**
+     * StockChartDTO를 StockChart 엔티티로 변환하는 메서드
+     *
+     * @param stock Stock 엔티티 (ManyToOne 관계)
+     * @return StockChart 엔티티
+     */
+    public StockChart toEntity(Stock stock) {
+//        LocalDateTime tradeDateTime;
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+//        try {
+//            tradeDateTime = LocalDateTime.parse(this.time, formatter);
+//        } catch (DateTimeParseException e) {
+//            System.err.println("Date parsing error: " + e.getMessage());
+//            tradeDateTime = LocalDateTime.now(); // 파싱 실패 시 현재 시간으로 대체 (필요 시 수정)
+//        }
+
+        return StockChart.builder()
+                .currentPrice(this.currentPrice)
+                .openPrice(this.openPrice)
+                .highPrice(this.highPrice)
+                .lowPrice(this.lowPrice)
+                .versusSign(this.versusSign)
+                .contingentVol(this.contingentVol)
+                .accumulatedVol(this.accumulatedVol)
+                .accumulatedAmount(this.accumulatedAmount)
+                .stock(stock)
+                .build();
+    }
 }
