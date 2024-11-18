173bb8850fc3e8bd24cedb7447eb65b4c031cccc
최수연 <tndus5383@gmail.com>
2024-11-18 11:21:48 +0900

    feat(#59): @PostConstruct로 종목 정보 추가

--- Coconut-Backend/src/main/java/com/coconut/stock_app/repository/cloud/StockRepository.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/repository/cloud/StockRepository.java
@@ -1,10 +1,11 @@
 package com.coconut.stock_app.repository.cloud;
 
 import com.coconut.stock_app.entity.cloud.Stock;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
-    Stock findByStockCode(String stockCode);
+    Optional<Stock> findByStockCode(String stockCode);
 }
