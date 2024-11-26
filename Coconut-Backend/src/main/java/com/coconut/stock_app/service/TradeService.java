69ca2bb73842d1f29f081543a059905efd652bcb
jiwon <pg15320@gmail.com>
2024-11-26 10:38:40 +0900

    fix-매수매도기능 JWT에서 uuid 가져오기

--- Coconut-Backend/src/main/java/com/coconut/stock_app/service/TradeService.java
+++ Coconut-Backend/src/main/java/com/coconut/stock_app/service/TradeService.java
@@ -1,10 +1,11 @@
 package com.coconut.stock_app.service;
 
 import com.coconut.stock_app.dto.trade.OrderDTO;
+import com.coconut.stock_app.entity.on_premise.Account;
 import com.coconut.stock_app.entity.on_premise.Order;
 
 public interface TradeService {
-    void processBuyOrder(OrderDTO buyOrder);
-    void processSellOrder(OrderDTO sellOrder);
+    void processBuyOrder(OrderDTO buyOrder, Account account);
+    void processSellOrder(OrderDTO sellOrder, Account account);
 
 }
